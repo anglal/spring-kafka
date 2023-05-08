f5de825869108abd5f6ac5d5a042f1b82985672d
anglal <lalshwarlimbu@gmail.com>
2023-05-08 15:12:50 -0700

    Added producer

---
+++ spring-kaka-producer/src/main/java/com/zlimbu/lal/springkakaproducer/SpringKakaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.zlimbu.lal.springkakaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKakaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKakaProducerApplication.class, args);
+	}
+
+}
